2fd3df319c3ac1af363c59ca460219ad1375eeb5
KHWX <tiago@antigravity.local>
2025-12-06 16:04:11 +0000

    Integrate localized Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import Layout from './Layout';
 import Dashboard from './components/Dashboard';
 import { ThemeProvider } from './contexts/ThemeContext';
 
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
     return (
         <ThemeProvider>
             <Layout>
                 <Dashboard />
+                <Analytics />
             </Layout>
         </ThemeProvider>
     );
